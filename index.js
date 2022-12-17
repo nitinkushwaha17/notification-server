70a7c5f49706acf703972a64626f1ad2870e3ea4
Nitin Kushwaha <nk17oct@gmail.com>
2022-12-17 18:47:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const webpush = require("web-push");
+const bodyParser = require("body-parser");
+const path = require("path");
+var cors = require('cors')
+ 
+const app = express();
+app.use(cors())
+
+// Set static path
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.use(bodyParser.json());
+
+const publicVapidKey =
+  "BBTySeWwsrm46Uqs2wzuQWsUIcy0Q4k9VKJZhj8O-VS65Wc75yWWzzNcEIKGwOLR2NF15vvQ3AAjDyGU7JbBORw";
+const privateVapidKey = "JMsh0hsPHdoC2eX1gmquMiqXTwGBcg8Rfkal10Bccec";
+
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+// Subscribe Route
+app.post("/subscribe", (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload
+  const payload = JSON.stringify({ title: "Push Test" });
+
+  // Pass object into sendNotification
+  webpush
+    .sendNotification(subscription, payload)
+    .catch(err => console.error(err));
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
