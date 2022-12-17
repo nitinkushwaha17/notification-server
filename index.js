45197fa14c7dd4487880c0993c847315e2ad3ba2
Nitin Kushwaha <nk17oct@gmail.com>
2022-12-17 18:53:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.post("/subscribe", (req, res) => {
     .catch(err => console.error(err));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
